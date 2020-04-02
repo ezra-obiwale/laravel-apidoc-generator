9b57d5453b85a0c3950e0c38a2c0332a11bbd29a
Ezra Obiwale <contact@ezraobiwale.com>
2020-04-02 02:14:23 +0100

    Wraps authentication label in a paragraph

--- resources/views/partials/route.blade.php
+++ resources/views/partials/route.blade.php
@@ -3,7 +3,7 @@
 @else## {{$route['uri']}}@endif
 @if($route['authenticated'])
 
-<br><small style="padding: 1px 9px 2px;font-weight: bold;white-space: nowrap;color: #ffffff;-webkit-border-radius: 9px;-moz-border-radius: 9px;border-radius: 9px;background-color: #3a87ad;">Requires authentication</small>@endif
+<p><small style="padding: 1px 9px 2px;font-weight: bold;white-space: nowrap;color: #ffffff;-webkit-border-radius: 9px;-moz-border-radius: 9px;border-radius: 9px;background-color: #3a87ad;">Requires authentication</small></p>@endif
 @if($route['description'])
 
 {!! $route['description'] !!}
